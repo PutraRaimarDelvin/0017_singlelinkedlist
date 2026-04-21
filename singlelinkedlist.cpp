67bfdd6d02be322dabb57bda9163ce332ff132e0
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-04-21 11:10:08 +0700

    menambahkan fungsi addNode untuk menambah data ke linked list

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -22,4 +22,34 @@ public:
     {
         return (START == NULL);
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+        nodeBaru->next = NULL;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
 };
